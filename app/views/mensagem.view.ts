1bf15d576637a0585150e971ec60d1b5293c516f
Gustavo Xavier <gus.guilhermexavier08@gmail.com>
2022-07-15 21:22:07 -0300

    Typescript-Curso-2 Capitulo-2 Aula-4

--- app/views/mensagem.view.ts
+++ app/views/mensagem.view.ts
@@ -1,10 +1,8 @@
-export class MensagemView{
-  
-    private  element: HTMLElement;
-    constructor(seletor:string){
-        this.element = document.querySelector(seletor);
-    }
+import { View } from "./view";
 
+export class MensagemView extends View{
+  
+ 
     template(model:string):string{
         return `
        <p class="alert alert-info">${model}</p> 
@@ -13,7 +11,7 @@ export class MensagemView{
 
     update(model:string):void{
     const template = this.template(model);
-    this.element.innerHTML = template;
+    this.elemento.innerHTML = template;
     }
 
 
